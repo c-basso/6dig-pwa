35a7c27f24df5be29163d73c9db451d0ff086b92
Владимир Ю. Ивахненко <ivakhnenko@yoomoney.ru>
2023-10-28 18:10:49 +0300

    wip

---
+++ tasks/logo.js
@@ -0,0 +1,35 @@
+const path = require('path');
+const sharp = require('sharp');
+const {takeHtmlPageScreenshot} = require('./takeHtmlPageScreenshot');
+
+const ICONS_PATH =  path.resolve(__dirname, '..', 'img');
+
+(async () => {
+    const htmlPath = path.resolve(__dirname, `logo.html`);
+    const screenshotPath = path.join(ICONS_PATH, `icon_1024.png`);
+    
+    const width = 1024;
+    const height = 1024;
+
+    await takeHtmlPageScreenshot({
+        htmlPath,
+        screenshotPath,
+        width,
+        height
+    });
+
+    const sizes = [
+        700,
+        144
+    ];
+
+    for (let size of sizes) {
+        await sharp(screenshotPath)
+            .png({
+                palette: true,
+                progressive: true
+            })
+            .resize(size, size)
+            .toFile(path.join(ICONS_PATH, `icon_${size}.png`));
+    }
+})()
